9d8ce26c3d0872aff21809e013af942c28b9f05c
quentin basset <quentin.basset@ochelys.com>
2018-01-30 12:05:15 +0100

    semicolon removed on promise for echo bot example

--- examples/echo-bot/index.js
+++ examples/echo-bot/index.js
@@ -21,7 +21,7 @@ const app = express();
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result));
+    .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
       res.status(500).end();
