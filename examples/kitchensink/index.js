b03a1a631816d7e110e519e925f05dfbe50dc1fa
Hyunje Jun <hyunje.jun@linecorp.com>
2018-11-13 19:52:18 +0900

    [kitchensink] Add example code for video in imagemap

--- examples/kitchensink/index.js
+++ examples/kitchensink/index.js
@@ -256,6 +256,20 @@ function handleText(message, replyToken, source) {
             { area: { x: 0, y: 520, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/play/en' },
             { area: { x: 520, y: 520, width: 520, height: 520 }, type: 'message', text: 'URANAI!' },
           ],
+          video: {
+            originalContentUrl: `${baseURL}/static/imagemap/video.mp4`,
+            previewImageUrl: `${baseURL}/static/imagemap/preview.jpg`,
+            area: {
+              x: 280,
+              y: 385,
+              width: 480,
+              height: 270,
+            },
+            externalLink: {
+              linkUri: 'https://line.me',
+              label: 'LINE'
+            }
+          },
         }
       );
     case 'bye':
