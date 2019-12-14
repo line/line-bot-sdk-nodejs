5eaebdc89ae0263e9d96f225e1d735b08b2decd9
xingoxu <xingoxu@gmail.com>
2019-12-15 00:17:31 +0900

    fix review

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -3,16 +3,18 @@ import { JSONParseError } from "../lib/exceptions";
 import { equal, ok } from "assert";
 
 describe("utils", () => {
-  it("fails when it's not an object", () => {
-    let input = "not Object";
-    try {
-      ensureJSON(input);
-      ok(false);
-    } catch (err) {
-      equal(
-        (err as JSONParseError).message,
-        "Failed to parse response body as JSON",
-      );
-    }
+  describe("ensureJSON", () => {
+    it("fails when input isn't an object", () => {
+      let input = "not Object";
+      try {
+        ensureJSON(input);
+        ok(false);
+      } catch (err) {
+        equal(
+          (err as JSONParseError).message,
+          "Failed to parse response body as JSON",
+        );
+      }
+    });
   });
 });
