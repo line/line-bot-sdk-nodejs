ee835b50809ecb9d7322dbd081cfa30f23205b36
xingoxu <xingoxu@gmail.com>
2019-12-15 00:10:34 +0900

    change let to const

--- lib/endpoints.ts
+++ lib/endpoints.ts
@@ -1,3 +1,3 @@
-export let MESSAGING_API_PREFIX = `https://api.line.me/v2/bot`;
-export let DATA_API_PREFIX = `https://api-data.line.me/v2/bot`;
-export let OAUTH_BASE_PREFIX = `https://api.line.me/v2/oauth`;
+export const MESSAGING_API_PREFIX = `https://api.line.me/v2/bot`;
+export const DATA_API_PREFIX = `https://api-data.line.me/v2/bot`;
+export const OAUTH_BASE_PREFIX = `https://api.line.me/v2/oauth`;
