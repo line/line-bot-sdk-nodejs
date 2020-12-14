401c3b3857095cc854c831dc5973c0282e685e56
Richard Lee <14349+dlackty@users.noreply.github.com>
2020-12-14 15:11:08 +0800

    Add missing sticker resource type

--- lib/types.ts
+++ lib/types.ts
@@ -479,7 +479,8 @@ export type StickerEventMessage = {
     | "ANIMATION_SOUND"
     | "POPUP"
     | "POPUP_SOUND"
-    | "NAME_TEXT";
+    | "NAME_TEXT"
+    | "PER_STICKER_TEXT";
   keywords: string[];
 } & EventMessageBase;
 
