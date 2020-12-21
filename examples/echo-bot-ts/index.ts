a964ebdacb381dd87c377585d7126f81592e303a
lauslim12 <nicholasdwiarto@yahoo.com>
2020-12-21 16:09:20 +0700

    feat: add example echo bot

---
+++ examples/echo-bot-ts/index.ts
@@ -0,0 +1,82 @@
+// Import all dependencies, mostly using destructuring for better view.
+import { ClientConfig, Client, middleware, MiddlewareConfig, WebhookEvent, TextMessage, MessageAPIResponseBase } from '@line/bot-sdk';
+import express, { Application, Request, Response } from 'express';
+
+// Setup all LINE client and Express configurations.
+const clientConfig: ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const middlewareConfig: MiddlewareConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET || '',
+};
+
+const PORT = process.env.PORT || 3000;
+
+// Create a new LINE SDK client.
+const client = new Client(clientConfig);
+
+// Create a new Express application.
+const app: Application = express();
+
+// Function handler to receive the text.
+const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
+  // Process all variables here.
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return;
+  }
+
+  // Process all message related variables here.
+  const { replyToken } = event;
+  const { text } = event.message;
+
+  // Create a new message.
+  const response: TextMessage = {
+    type: 'text',
+    text,
+  };
+
+  // Reply to the user.
+  return client.replyMessage(replyToken, response);
+};
+
+// Register the LINE middleware.
+// As an alternative, you could also pass the middleware in the route handler.
+app.use(middleware(middlewareConfig));
+
+// Route handler to receive webhook events.
+app.post(
+  '/webhook',
+  async (req: Request, res: Response): Promise<Response> => {
+    const events: WebhookEvent[] = req.body.events;
+
+    // Process all of the received events asynchronously.
+    const results = events.map((event: WebhookEvent) => {
+      try {
+        textEventHandler(event);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error(err);
+        }
+
+        // Return an error message.
+        return res.status(500).json({
+          status: 'error',
+        });
+      }
+    });
+
+    // Return a successfull message.
+    return res.status(200).json({
+      status: 'success',
+      results,
+    });
+  }
+);
+
+// Create a server and listen to it.
+app.listen(PORT, () => {
+  console.log(`Application is live and listening on port ${PORT}`);
+});
