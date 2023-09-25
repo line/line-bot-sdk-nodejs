860caff1ed3fb657ad5f2b95c9400a6f187fcd2f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-09-26 07:30:36 +0900

    Fix test cases in the generator

--- generator/src/test/java/line/bot/generator/LineBotSdkNodejsGeneratorGeneratorTest.java
+++ generator/src/test/java/line/bot/generator/LineBotSdkNodejsGeneratorGeneratorTest.java
@@ -19,26 +19,26 @@ import static org.junit.Assert.assertEquals;
  */
 public class LineBotSdkNodejsGeneratorGeneratorTest {
 
-  // use this test to launch you code generator in the debugger.
-  // this allows you to easily set break points in MyclientcodegenGenerator.
-  @Test
-  public void launchCodeGenerator() {
-    // to understand how the 'openapi-generator-cli' module is using 'CodegenConfigurator', have a look at the 'Generate' class:
-    // https://github.com/OpenAPITools/openapi-generator/blob/master/modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Generate.java
-    final CodegenConfigurator configurator = new CodegenConfigurator()
-              .setGeneratorName("line-bot-sdk-nodejs-generator") // use this codegen library
-              .setInputSpec("../../../modules/openapi-generator/src/test/resources/2_0/petstore.yaml") // sample OpenAPI file
-              // .setInputSpec("https://raw.githubusercontent.com/openapitools/openapi-generator/master/modules/openapi-generator/src/test/resources/2_0/petstore.yaml") // or from the server
-              .setOutputDir("out/line-bot-sdk-nodejs-generator"); // output directory
+    // use this test to launch you code generator in the debugger.
+    // this allows you to easily set break points in MyclientcodegenGenerator.
+    @Test
+    public void launchCodeGenerator() {
+        // to understand how the 'openapi-generator-cli' module is using 'CodegenConfigurator', have a look at the 'Generate' class:
+        // https://github.com/OpenAPITools/openapi-generator/blob/master/modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Generate.java
+        final CodegenConfigurator configurator = new CodegenConfigurator()
+                .setGeneratorName("line-bot-sdk-nodejs-generator") // use this codegen library
+                .setInputSpec("../line-openapi/shop.yml") // sample OpenAPI file
+                // .setInputSpec("https://raw.githubusercontent.com/openapitools/openapi-generator/master/modules/openapi-generator/src/test/resources/2_0/petstore.yaml") // or from the server
+                .setOutputDir("out/line-bot-sdk-nodejs-generator"); // output directory
 
-    final ClientOptInput clientOptInput = configurator.toClientOptInput();
-    DefaultGenerator generator = new DefaultGenerator();
-    generator.opts(clientOptInput).generate();
-  }
+        final ClientOptInput clientOptInput = configurator.toClientOptInput();
+        DefaultGenerator generator = new DefaultGenerator();
+        generator.opts(clientOptInput).generate();
+    }
 
     @Test
     public void pathReplacer() {
-      String s = LineBotSdkNodejsGeneratorGenerator.pathReplacer("/foo/{bar}/baz/{boz}");
-      assertEquals(".replace(\"{bar}\", String(bar)).replace(\"{boz}\", String(boz))", s);
+        String s = LineBotSdkNodejsGeneratorGenerator.pathReplacer("/foo/{bar}/baz/{boz}");
+        assertEquals(".replace(\"{bar}\", String(bar)).replace(\"{boz}\", String(boz))", s);
     }
 }
