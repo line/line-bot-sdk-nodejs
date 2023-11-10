494bda6f779cc9dd38b3525324fb2b3d1d51ab0b
Tokuhiro Matsuno <tokuhirom@gmail.com>
2023-11-10 10:03:31 +0900

    msw to node's http library: libs-messagingApi.spec.ts

--- test/libs-messagingApi.spec.ts
+++ test/libs-messagingApi.spec.ts
@@ -1,55 +1,51 @@
-import { messagingApi } from "../lib";
-import { http, HttpResponse } from "msw";
-import { setupServer } from "msw/node";
-import { deepEqual, equal } from "assert";
+import { messagingApi, shop } from "../lib";
+import { createServer } from "http";
+import { deepEqual, equal, ok } from "assert";
 
 const pkg = require("../package.json");
 
 const channelAccessToken = "test_channel_access_token";
 
-const client = new messagingApi.MessagingApiClient({
-  channelAccessToken,
-});
-
-const blobClient = new messagingApi.MessagingApiBlobClient({
-  channelAccessToken,
-});
 
 describe("messagingApi", () => {
-  const server = setupServer();
-  before(() => {
-    server.listen();
-  });
-  after(() => {
-    server.close();
-  });
-  afterEach(() => {
-    server.resetHandlers();
-  });
-
   it("setRichMenuImage", async () => {
     let requestCount = 0;
-    server.use(
-      http.post(
-        "https://api-data.line.me/v2/bot/richmenu/aaaaaa/content",
-        async ({ request, params, cookies }) => {
-          requestCount++;
 
-          equal(
-            request.headers.get("Authorization"),
-            `Bearer test_channel_access_token`,
-          );
-          equal(
-            request.headers.get("User-Agent"),
-            `${pkg.name}/${pkg.version}`,
-          );
-          equal(request.headers.get("content-type"), "image/jpeg");
-          equal(await request.text(), "GREAT_JPEG");
+    const server = createServer((req, res) => {
+      requestCount++;
 
-          return HttpResponse.json({});
-        },
-      ),
-    );
+      equal(req.url, "/v2/bot/richmenu/aaaaaa/content");
+
+      equal(req.headers.authorization, "Bearer test_channel_access_token");
+      equal(req.headers["user-agent"], `${pkg.name}/${pkg.version}`);
+      equal(req.headers["content-type"], "image/jpeg");
+
+      let body = "";
+      req.on("data", chunk => {
+        body += chunk;
+      });
+
+      req.on("end", () => {
+        equal(body, "GREAT_JPEG");
+
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(JSON.stringify({}));
+      });
+    });
+    await new Promise(resolve => {
+      server.listen(0);
+      server.on("listening", resolve);
+    });
+
+    const serverAddress = server.address();
+    if (typeof serverAddress === "string" || serverAddress === null) {
+      throw new Error("Unexpected server address: " + serverAddress);
+    }
+
+    const blobClient = new messagingApi.MessagingApiBlobClient({
+      channelAccessToken,
+      baseURL: `http://localhost:${String(serverAddress.port)}/`,
+    });
 
     const res = await blobClient.setRichMenuImage(
       "aaaaaa",
@@ -57,30 +53,49 @@ describe("messagingApi", () => {
     );
     equal(requestCount, 1);
     deepEqual(res, {});
+
+    server.close();
   });
 
   it("pushMessage", async () => {
     let requestCount = 0;
-    server.use(
-      http.post(
-        "https://api.line.me/v2/bot/message/push",
-        ({ request, params, cookies }) => {
-          requestCount++;
 
-          equal(
-            request.headers.get("Authorization"),
-            "Bearer test_channel_access_token",
-          );
-          equal(
-            request.headers.get("User-Agent"),
-            `${pkg.name}/${pkg.version}`,
-          );
-          equal(request.headers.get("content-type"), "application/json");
-          equal(request.headers.get("x-line-retry-key"), "KEYKEYKEYKEY");
-          return HttpResponse.json({});
-        },
-      ),
-    );
+    const server = createServer((req, res) => {
+      requestCount++;
+
+      equal(req.url, "/v2/bot/message/push");
+
+      equal(req.headers.authorization, "Bearer test_channel_access_token");
+      equal(req.headers["user-agent"], `${pkg.name}/${pkg.version}`);
+      equal(req.headers["content-type"], "application/json");
+      equal(req.headers["x-line-retry-key"], "KEYKEYKEYKEY");
+
+      let body = "";
+      req.on("data", chunk => {
+        body += chunk;
+      });
+
+      req.on("end", () => {
+        ok(body.includes("uAAAAAAAAAAAAAA"));
+
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(JSON.stringify({}));
+      });
+    });
+    await new Promise(resolve => {
+      server.listen(0);
+      server.on("listening", resolve);
+    });
+
+    const serverAddress = server.address();
+    if (typeof serverAddress === "string" || serverAddress === null) {
+      throw new Error("Unexpected server address: " + serverAddress);
+    }
+
+    const client = new messagingApi.MessagingApiClient({
+      channelAccessToken,
+      baseURL: `http://localhost:${String(serverAddress.port)}/`,
+    });
 
     const res = await client.pushMessage(
       { to: "uAAAAAAAAAAAAAA", messages: [{ type: "text", text: "aaaaaa" }] },
@@ -88,5 +103,6 @@ describe("messagingApi", () => {
     );
     equal(requestCount, 1);
     deepEqual(res, {});
+    server.close();
   });
 });
