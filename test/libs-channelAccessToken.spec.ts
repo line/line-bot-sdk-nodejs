72817ca54064b75555a0784622d0a8e347fa4590
Yuta Kasai <yuta.kasai@linecorp.com>
2023-11-20 16:10:02 +0900

    NO-ISSUE Fix form parameter and delete it only when undefined

--- test/libs-channelAccessToken.spec.ts
+++ test/libs-channelAccessToken.spec.ts
@@ -34,7 +34,7 @@ describe("channelAccessToken", () => {
           );
           equal(
             await request.text(),
-            "grantType=test_client_id&clientAssertionType=test_client_secret&clientAssertion=test_grant_type&clientId=test_redirect_uri&clientSecret=test_code",
+            "grant_type=test_client_id&client_id=1234&client_secret=test_code",
           );
 
           return HttpResponse.json({});
@@ -44,9 +44,9 @@ describe("channelAccessToken", () => {
 
     const res = await client.issueStatelessChannelToken(
       "test_client_id",
-      "test_client_secret",
-      "test_grant_type",
-      "test_redirect_uri",
+      undefined,
+      undefined,
+      "1234",
       "test_code",
     );
     deepEqual(res, {});
