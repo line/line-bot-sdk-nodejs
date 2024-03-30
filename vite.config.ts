f761bd01e7bf264d803acfbc100dce0a829933c2
Yuta Kasai <kasai.yuta0810@gmail.com>
2024-03-30 22:03:18 +0900

    Add vite test config

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        include: [
+            "test/**/*.spec.ts",
+            "lib/**/tests/**/*.spec.ts"
+        ]
+    },
+})
