09c61dbce65f1a98a2526f8630de16b20ecc9e06
Yuta Kasai <kasai.yuta0810@gmail.com>
2024-04-02 09:48:13 +0900

    Support CJS again (#797)
    We will also support CJS to make it a dual package. It seems that for
    Typescript, it needs to be version 5.3 or higher.
    
    Additionally, I have introduced two validators for the dual package.
    1. publint https://publint.dev/
    2. arethetypeswrong https://arethetypeswrong.github.io/

--- examples/echo-bot-ts-cjs/index.ts
+++ examples/echo-bot-ts-cjs/index.ts
@@ -16,7 +16,6 @@ const clientConfig: ClientConfig = {
 };
 
 const middlewareConfig: MiddlewareConfig = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET || '',
 };
 
