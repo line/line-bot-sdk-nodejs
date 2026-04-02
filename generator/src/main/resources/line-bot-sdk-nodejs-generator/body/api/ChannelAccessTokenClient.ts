c8c3acc3c967ef6fb5b2ca0cc10e2fd07930b43e
habara keigo <habara.keigo@lycorp.co.jp>
2026-04-02 19:13:47 +0900

    Support *_with_http_info

--- generator/src/main/resources/line-bot-sdk-nodejs-generator/body/api/ChannelAccessTokenClient.ts
+++ generator/src/main/resources/line-bot-sdk-nodejs-generator/body/api/ChannelAccessTokenClient.ts
@@ -20,3 +20,25 @@
             clientSecret,
         );
     }
+    public async issueStatelessChannelTokenByJWTAssertionWithHttpInfo(
+        clientAssertion: string,
+    ): Promise<Types.ApiResponseType<IssueStatelessChannelAccessTokenResponse>> {
+        return this.issueStatelessChannelTokenWithHttpInfo(
+            "client_credentials",
+            "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+            clientAssertion,
+        );
+    }
+
+    public async issueStatelessChannelTokenByClientSecretWithHttpInfo(
+        clientId: string,
+        clientSecret: string,
+    ): Promise<Types.ApiResponseType<IssueStatelessChannelAccessTokenResponse>> {
+        return this.issueStatelessChannelTokenWithHttpInfo(
+            "client_credentials",
+            undefined,
+            undefined,
+            clientId,
+            clientSecret,
+        );
+    }
